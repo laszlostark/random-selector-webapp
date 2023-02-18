aa4e4d548cc2a61ab74e41f685b173bee797f0bb
Laszlo Stark <klimsalabimm@gmail.com>
2023-02-18 12:26:06 +0100

    changed storage code in anticipation for settings screen

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 const loadListButton = document.getElementById("load-list-button");
 const loadListDropdown = document.getElementById("load-list-dropdown");
 const mainList = document.getElementById("main-list");
-const addToMainListButton = document.getElementById("add-to-main-list-button");
 const addToMainListInput = document.getElementById("add-to-main-list-input");
 const addToMainListForm = document.getElementById("add-to-main-list-form");
-const saveAsDropdown = document.getElementById("save-as-dropdown");
-const saveAsInput = document.getElementById("save-as-input");
-const saveAsButton = document.getElementById("save-as-button");
-const saveAsForm = document.getElementById("save-as-form");
 const randomDialogue = document.getElementById("random-dialogue");
 const randomDrawButton = document.getElementById("random-draw-button");
 const randomOutputField = document.getElementById("random-output-field");
 const randomTypeSelect = document.getElementById("random-type-select");
-const listNameField = document.getElementById("list-name");
+const listNameInput = document.getElementById("list-name-input");
 
 var storageList = { lists: [] };
 var currentList = [];
 var currentListName;
+var currentListGUID;
 var currentHat;
 
 init();
 
 
 function init() {
-  saveAsDropdown.value = "none";
   loadListDropdown.value = "none";
   loadListButton.disabled = true;
+  listNameInput.value = "";
   parseStorageList();
+  currentListGUID = crypto.randomUUID();
 }
 
 function drawFromHat() {
@@ -53,50 +50,42 @@ function randomInt(max) {
 
 function setListNameDisplay(name) {
   if(!name) {
-    listNameField.textContent = "";
-    listNameField.style = "display: none;";
+    listNameInput.value = "";
   }
-  listNameField.textContent = name;
-  listNameField.style = "display: default;"
+  
+  listNameInput.value = name;
 }
 
-function saveAs() {
-  if (saveAsButton.classList.contains("btn-danger")) {
-    saveAsButton.classList.remove("btn-danger");
-    saveAsButton.classList.add("btn-outline-primary");
-    storageList.lists[saveAsDropdown.value].items = currentList;
-    localStorage.list = JSON.stringify(storageList);
-    saveAsDropdown.value = "none";
-  }
-
-  switch (saveAsDropdown.value) {
-    case "none":
+function save(listUID) {
+  if(currentList.length < 1) return;
+  let list;
+  for(var i = 0 ; i < storageList.lists.length ; i++) {
+    if(storageList.lists[i].GUID == listUID) {
+      list = i;
       break;
-    case "new":
-      if (saveAsInput) {
-        storageList.lists.push({
-          "name": saveAsInput.value,
-          "items": currentList
-        });
-        currentListName = saveAsInput.value;
-        setListNameDisplay(currentListName);
-        saveAsInput.value = "";
-        saveAsInput.disabled = true;
-        saveAsDropdown.value = "none";
-        localStorage.list = JSON.stringify(storageList);
-        parseStorageList();
-      }
-      break;
-    default:
-      saveAsButton.classList.remove("btn-outline-primary");
-      saveAsButton.classList.add("btn-danger");
+    }
   }
+  if(list) {
+    storageList.lists[list].items = currentList;
+    storageList.lists[list].name = listNameInput.value;
+  } else {
+    storageList.lists.push({
+      GUID: listUID,
+      name: listNameInput.value,
+      items: currentList
+    });
+  }
+  localStorage.list = JSON.stringify(storageList);
+  parseStorageList();
 }
 
 function addToMainList() {
   addListItem(addToMainListInput.value);
   if(currentList) {
     randomDialogue.style="display: default;";
+  }
+  if(currentListName) {
+    save(currentListGUID);
   }
   addToMainListInput.value = "";
 }
@@ -118,41 +107,29 @@ function addListViewOption(text, value) {
   listOption.value = value;
   listOption.append(optionText);
   loadListDropdown.appendChild(listOption);
-  saveAsDropdown.appendChild(listOption.cloneNode(true));
 }
 
 function refreshList() {
   mainList.textContent = '';
-  for (i in currentList) {
-    let text = document.createTextNode(`${parseInt(i) + 1}. ${currentList[i]}`);
+  //for (i in currentList) {
+  currentList.forEach((e,i) => {
+    let text = document.createTextNode(`${parseInt(i) + 1}. ${e}`);
     let item = document.createElement('li');
     item.classList.add("list-group-item");
     item.appendChild(text);
     mainList.appendChild(item);
-    setListNameDisplay(currentListName);
-  }
+  })
+  setListNameDisplay(currentListName);
 }
 
 function populateListDefaults() {
   loadListDropdown.textContent = "";
-  saveAsDropdown.textContent = "";
   let loadOption = document.createElement('option');
-  let saveOptionDefault = document.createElement('option');
-  let saveOptionNew = document.createElement('option');
   loadOption.disabled = true;
-  saveOptionDefault.disabled = true;
   loadOption.value = "none";
-  saveOptionDefault.value = "none";
-  saveOptionNew.value = "new";
   loadOption.textContent = "Load list..."
-  saveOptionDefault.textContent = "Save as..."
-  saveOptionNew.textContent = "New list..."
-  saveAsDropdown.appendChild(saveOptionDefault);
-  saveAsDropdown.appendChild(saveOptionNew);
-  saveAsDropdown.value = "none";
   loadListDropdown.appendChild(loadOption);
   loadListDropdown.value = "none";
-  saveAsInput.style = "display: none;"
 }
 
 function parseStorageList() {
@@ -161,15 +138,33 @@ function parseStorageList() {
   }
   populateListDefaults();
   if (!localStorage.list) return;
-  for (i in storageList.lists) {
-    addListViewOption(storageList.lists[i].name, i);
-  }
+  storageList.lists.forEach((e, index) => {
+    addListViewOption(e.name, index);
+  })
   loadListDropdown.disabled = false;
+}
+
+function loadList(index) {
+  if(index === undefined) {
+    index = loadListDropdown.value;
+  }
+  currentList = storageList.lists[index].items;
+  currentListName = storageList.lists[index].name;
+  currentListGUID = storageList.lists[index].GUID;
+  randomDialogue.style = "display: default;";
+  refreshList();
 }
 
 
 // These are all event listeners
+listNameInput.addEventListener("change", () => {
+  if(listNameInput.value) {
+    currentListName = listNameInput.value;
+  }
+})
+
 randomDrawButton.addEventListener("click", () => {
+  randomOutputField.parentElement.style = "height: 4.5em; display: default;";
   switch(randomTypeSelect.value) {
     case "random-true":
       randomOutputField.textContent = drawTrueRandom();
@@ -188,32 +183,8 @@ loadListDropdown.addEventListener("change", () => {
   }
 })
 
-saveAsDropdown.addEventListener("change", () => {
-  if (saveAsButton.classList.contains("btn-danger")) {
-    saveAsButton.classList.remove("btn-danger");
-    saveAsButton.classList.add("btn-outline-primary");
-  }
-
-  if (saveAsDropdown.value == "new") {
-    saveAsInput.style = ""
-    saveAsButton.disabled = false;
-  } else {
-    saveAsInput.style = "display: none"
-    saveAsButton.disabled = false;
-  }
-})
-
-saveAsForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  saveAs();
-
-});
-
 loadListButton.addEventListener("click", () => {
-  currentList = storageList.lists[loadListDropdown.value].items;
-  currentListName = storageList.lists[loadListDropdown.value].name;
-  randomDialogue.style = "display: default;";
-  refreshList();
+  loadList();
 });
 
 addToMainListForm.addEventListener("submit", (e) => {
