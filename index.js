795cbc1794cd4f6b294fba5a7605146e11f6cb09
Laszlo Stark <klimsalabimm@gmail.com>
2023-02-19 11:26:40 +0100

    started implementation of edit view

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const randomDrawButton = document.getElementById("random-draw-button");
 const randomOutputField = document.getElementById("random-output-field");
 const randomTypeSelect = document.getElementById("random-type-select");
 const listNameInput = document.getElementById("list-name-input");
-const listSettingsButton = document.getElementById("list-settings-button");
+const listEditButton = document.getElementById("list-edit-button");
 const saveConfirmButton = document.getElementById("save-confirm-button");
 const saveAbortButton = document.getElementById("save-abort-button");
+const editView = document.getElementById("edit-view");
+const editList = document.getElementById("edit-list");
+const editConfirmButton = document.getElementById("edit-confirm-button");
+const editAbortButton = document.getElementById("edit-abort-button");
+const editListNameInput = document.getElementById("edit-list-name-input");
+const mainListView = document.getElementById("main-list-view");
 
 var storageList = { lists: [] };
 var currentList = [];
@@ -18,6 +24,7 @@ var currentListName;
 var currentListGUID;
 var currentListIndex = "none";
 var currentHat;
+var workingListCopy;
 
 init();
 
@@ -27,18 +34,17 @@ function init() {
   loadListButton.disabled = true;
   listNameInput.value = "";
   parseStorageList();
-  currentListGUID = crypto.randomUUID();
 }
 
 function drawFromHat() {
-  if(!currentHat) {
-    currentHat = currentList.slice();
-  } else if(!currentList) return;
+  if (!currentHat) {
+    currentHat = currentList.slice(0);
+  } else if (!currentList) {return};
   let randomNum = randomInt(currentHat.length);
   let randomItem = currentHat[randomNum];
   currentHat.splice(randomNum, 1);
-  if(currentHat.length < 1) {
-    currentHat = currentList.slice();
+  if (currentHat.length < 1) {
+    currentHat = currentList.slice(0);
   }
   return randomItem;
 }
@@ -53,25 +59,27 @@ function randomInt(max) {
 }
 
 function setListNameDisplay(name) {
-  if(!name) {
+  if (!name) {
     listNameInput.value = "";
   }
-  
+
   listNameInput.value = name;
 }
 
 function findListIndex(UID) {
-  for(var i = 0 ; i < storageList.lists.length ; i++) {
-    if(storageList.lists[i].GUID == UID) {
+  for (var i = 0; i < storageList.lists.length; i++) {
+    if (storageList.lists[i].GUID == UID) {
       return i;
     }
   }
 }
 
 function save(listUID) {
-  if(currentList.length < 1) return;
+  if (!currentListGUID) {
+    currentListGUID = listUID;
+  }
   let list = findListIndex(listUID);
-  if(list != undefined && list != NaN) {
+  if (list != undefined && list != NaN) {
     storageList.lists[list].items = currentList;
     storageList.lists[list].name = listNameInput.value;
   } else {
@@ -83,7 +91,7 @@ function save(listUID) {
   }
   localStorage.list = JSON.stringify(storageList);
   let newIndex = findListIndex(currentListGUID);
-  if(newIndex != NaN && newIndex != undefined) {
+  if (newIndex != NaN && newIndex != undefined) {
     currentListIndex = newIndex;
   }
   parseStorageList();
@@ -91,17 +99,17 @@ function save(listUID) {
 
 function addToMainList() {
   addListItem(addToMainListInput.value);
-  if(currentList) {
-    randomDialogue.style="";
+  if (currentList) {
+    show(randomDialogue);
   }
-  if(currentListName) {
+  if (currentListGUID) {
     save(currentListGUID);
   }
   addToMainListInput.value = "";
 }
 
 function addListItem(newItem) {
-  if (!newItem) return;
+  if (!newItem) {return};
   currentList.push(newItem);
   let listItem = document.createElement('li');
   listItem.classList.add("list-group-item");
@@ -111,7 +119,7 @@ function addListItem(newItem) {
 }
 
 function addListViewOption(text, value) {
-  if (!text || value == NaN || value == undefined) return;
+  if (!text || value == NaN || value == undefined) {return};
   let listOption = document.createElement('option');
   let optionText = document.createTextNode(text);
   listOption.value = value;
@@ -123,7 +131,7 @@ function refreshList() {
   loadListDropdown.value = currentListIndex;
   mainList.textContent = '';
   //for (i in currentList) {
-  currentList.forEach((e,i) => {
+  currentList.forEach((e, i) => {
     let text = document.createTextNode(`${parseInt(i) + 1}. ${e}`);
     let item = document.createElement('li');
     item.classList.add("list-group-item");
@@ -131,6 +139,23 @@ function refreshList() {
     mainList.appendChild(item);
   })
   setListNameDisplay(currentListName);
+}
+
+function populateEditView() {
+  editList.textContent = "";
+  workingListCopy = currentList.slice(0);
+  workingListCopy.forEach((e, i) => {
+    let text = document.createTextNode(`${parseInt(i) + 1}. ${e}`);
+    let checkbox = document.createElement("input");
+    checkbox.classList.add("btn-check");
+    checkbox.type = "checkbox";
+    checkbox.id = `edit-list-select-${i}`;
+    let item = document.createElement("li");
+    item.classList.add("list-group-item");
+    item.appendChild(text);
+    editList.appendChild(item);
+  })
+  editListNameInput.value = currentListName;
 }
 
 function populateListDefaults() {
@@ -148,7 +173,7 @@ function parseStorageList() {
     storageList = JSON.parse(localStorage.list);
   }
   populateListDefaults();
-  if (!localStorage.list) return;
+  if (!localStorage.list) {return};
   storageList.lists.forEach((e, index) => {
     addListViewOption(e.name, index);
   })
@@ -157,53 +182,101 @@ function parseStorageList() {
 }
 
 function loadList(index) {
-  if(index === undefined) {
+  if (index === undefined) {
     index = loadListDropdown.value;
   }
   currentList = storageList.lists[index].items;
   currentListName = storageList.lists[index].name;
   currentListGUID = storageList.lists[index].GUID;
-  randomDialogue.style = "";
+  if (currentList.length > 0) {show(randomDialogue)};
   currentListIndex = index;
   refreshList();
 }
 
+function hide(element) {
+  if (!element) {return};
+  element.classList.add("hide");
+}
 
-// These are all event listeners
-saveConfirmButton.addEventListener("click", () => {
-  if(listNameInput.value) {
+function show(element) {
+  if (!element) {return};
+  element.classList.remove("hide");
+}
+
+function saveConfirm() {
+  if (!currentListGUID) {
+    currentListGUID = crypto.randomUUID();
+  }
+  if (listNameInput.value) {
     currentListName = listNameInput.value;
     save(currentListGUID);
     listNameInput.value = currentListName;
-    listNameInput.dispatchEvent(new Event('input', {bubbles:true}));
+    listNameInput.dispatchEvent(new Event('input', { bubbles: true }));
   }
-})
+}
 
-saveAbortButton.addEventListener("click", () => {
-  if(currentListName) {
+function saveAbort() {
+  if (currentListName) {
     listNameInput.value = currentListName;
-    listNameInput.dispatchEvent(new Event('input', {bubbles:true}));
+    listNameInput.dispatchEvent(new Event('input', { bubbles: true }));
   } else {
     listNameInput.value = "";
-    listNameInput.dispatchEvent(new Event('input', {bubbles:true}));
+    listNameInput.dispatchEvent(new Event('input', { bubbles: true }));
   }
-})
+}
+
+// These are all event listeners
+editConfirmButton.addEventListener("click", () => {
+  currentList = workingListCopy;
+  currentListName = editListNameInput.value;
+  workingListCopy = undefined;
+  save(currentListGUID);
+  hide(editView);
+  show(mainListView);
+});
+
+editAbortButton.addEventListener("click", () => {
+  workingListCopy = undefined;
+  hide(editView);
+  show(mainListView);
+});
+
+listEditButton.addEventListener("click", () => {
+  if (!currentListGUID) {return};
+  populateEditView();
+  hide(mainListView);
+  show(editView);
+});
+
+saveConfirmButton.addEventListener("click", () => {
+  saveConfirm();
+});
+
+saveAbortButton.addEventListener("click", () => {
+  saveAbort();
+});
 
 listNameInput.addEventListener("input", () => {
-  if(listNameInput.value != currentListName && listNameInput.value) {
-    listSettingsButton.style = "display: none;"
-    saveConfirmButton.style = "";
-    saveAbortButton.style = "";
+  if (listNameInput.value != currentListName && listNameInput.value) {
+    hide(listEditButton);
+    show(saveConfirmButton);
+    show(saveAbortButton);
   } else {
-    saveConfirmButton.style = "display: none;"
-    saveAbortButton.style = "display: none;"
-    listSettingsButton.style = "width: 4em;";
+    show(listEditButton);
+    hide(saveConfirmButton);
+    hide(saveAbortButton);
   }
+});
+
+listNameInput.addEventListener("keyup", (ev) => {
+  if (ev.key == "Enter") {
+    saveConfirm();
+  } else if (ev.key == "Escape") { saveAbort() };
 })
 
 randomDrawButton.addEventListener("click", () => {
-  randomOutputField.parentElement.style = "height: 4.5em;";
-  switch(randomTypeSelect.value) {
+  show(randomOutputField);
+  switch (randomTypeSelect.value) {
     case "random-true":
       randomOutputField.textContent = drawTrueRandom();
       break;
@@ -211,7 +284,7 @@ randomDrawButton.addEventListener("click", () => {
       randomOutputField.textContent = drawFromHat();
       break;
   }
-})
+});
 
 loadListDropdown.addEventListener("change", () => {
   if (loadListDropdown.value == "none") {
@@ -219,11 +292,17 @@ loadListDropdown.addEventListener("change", () => {
   } else {
     loadListButton.disabled = false;
   }
-})
+});
 
 loadListButton.addEventListener("click", () => {
   loadList();
 });
+
+addToMainListInput.addEventListener("keyup", (ev) => {
+  if(ev.key == "Escape") {
+    addToMainListInput.value = "";
+  }
+})
 
 addToMainListForm.addEventListener("submit", (e) => {
   e.preventDefault();
